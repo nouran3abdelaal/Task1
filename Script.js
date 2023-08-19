16522d4cdc7ab6532ca628eaad0b88df9fabaf2a
nouran3abdelaal <nourakhaled329@gmail.com>
2023-08-19 13:58:25 +0300

    2nd commit button needs fix

---
+++ Script.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    // Add scroll event listener
+    console.log("in");
+  
+    window.addEventListener('scroll', function() {
+        console.log(button);
+      var button = document.getElementById('movingButton');
+      console.log(button);
+  
+      var scrollPosition = window.scrollX;
+      console.log(button);
+  
+      if (scrollPosition > 0) {
+        button.classList.add('moving');
+      } else {
+        button.classList.remove('moving');
+      }
+    });
+  };
